8f3688c9b1b5510aa38610de967959d6b7c6b6a7
mirmannan99 <mirmannan99@gmail.com>
2023-03-28 13:00:15 +0530

    first commit

---
+++ config-aws.js
@@ -0,0 +1,4 @@
+module.exports.accessKeyId = "AKIASC3VKIHAHJQ3GT77"; //TODO: Update
+module.exports.secretAccessKey = "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"; //TODO: Update
+module.exports.bucketName = "cargo-nerd-test"; //TODO: Update
+module.exports.region = "Asia Pacific (Tokyo) ap-northeast-1"; //TODO: Update
